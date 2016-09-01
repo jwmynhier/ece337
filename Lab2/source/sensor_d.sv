86ac29a487744507ea85765af3f2b8cca8c183f9
Mentor Graphics <mg111@ee215lnx08.ecn.purdue.edu>
2016-09-01 15:22:21 -0400

    Commit through 1.2.2.2

--- Lab2/source/sensor_d.sv
+++ Lab2/source/sensor_d.sv
@@ -5,3 +5,17 @@
 // Lab Section: 4
 // Version:     1.0  Initial Design Entry
 // Description: Dataflow Style sensor Error Detector.
+module sensor_d
+(
+	input wire [3:0] sensors,
+	output wire error
+);
+
+wire OneAndThree, OneAndTwo;
+
+assign OneAndThree = sensors[1] & sensors[3];
+assign OneAndTwo = sensors[1] & sensors[2];
+
+assign error = sensors[0] | OneAndThree | OneAndTwo;
+
+endmodule
