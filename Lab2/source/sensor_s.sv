e61979f82f81a1b185e04e745d6b9207aa7339b0
Mentor Graphics <mg111@ee215lnx08.ecn.purdue.edu>
2016-09-01 14:43:15 -0400

    Initial commit for Lab2

---
+++ Lab2/source/sensor_s.sv
@@ -0,0 +1,7 @@
+// $Id: $
+// File name:   sensor_s.sv
+// Created:     9/1/2016
+// Author:      Joseph Mynhier
+// Lab Section: 4
+// Version:     1.0  Initial Design Entry
+// Description: Sensor Error Detector Design
