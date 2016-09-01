01eea4d057ee1208cd349a9d93ad8552277f9342
Mentor Graphics <mg111@ee215lnx08.ecn.purdue.edu>
2016-09-01 15:31:12 -0400

    Commit through 1.2.3.2

--- Lab2/source/sensor_b.sv
+++ Lab2/source/sensor_b.sv
@@ -5,3 +5,18 @@
 // Lab Section: 4
 // Version:     1.0  Initial Design Entry
 // Description: Behavioral Style Sensor Error Detector
+module sensor_b
+(
+	input wire [3:0] sensors,
+	output reg error
+);
+
+	reg OneAndThree, OneAndTwo;
+
+	always_comb
+	begin
+		OneAndThree = sensors[1] & sensors[3];
+		OneAndTwo = sensors[1] & sensors[2];
+		error = sensors[0] | OneAndThree | OneAndTwo;
+	end
+endmodule
