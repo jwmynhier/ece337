e1aea11dfdb07a8b167291400e3ad18e25cffdc7
Mentor Graphics <mg111@ee215lnx08.ecn.purdue.edu>
2016-09-01 15:24:50 -0400

    Initial commit of sensor_b.sv

---
+++ Lab2/source/sensor_b.sv
@@ -0,0 +1,7 @@
+// $Id: $
+// File name:   sensor_b.sv
+// Created:     9/1/2016
+// Author:      Joseph Mynhier
+// Lab Section: 4
+// Version:     1.0  Initial Design Entry
+// Description: Behavioral Style Sensor Error Detector
