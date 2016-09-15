a991d8c1489bc2a3912a9771f2f553cba02a2581
Mentor Graphics <mg111@ee215lnx08.ecn.purdue.edu>
2016-09-15 17:26:54 -0400

    Commit in progress version of (tb_)moore.sv

--- Lab4/source/moore.sv
+++ Lab4/source/moore.sv
@@ -13,5 +13,109 @@ module moore
 	input wire i,
 	output reg o
 );
+	localparam NUM_STATE_BITS = 3;
+	typedef enum logic [NUM_STATE_BITS-1:0] 
+		{	HAS0,
+			HAS1,
+			HAS10,
+			HAS101,
+			HAS1011 } fsr_state;
+
+	fsr_state next_state;
+	fsr_state curr_state;
+
+	// Store the state.
+	always_ff @ (posedge clk, negedge n_rst)
+	begin
+		if (n_rst == '0)
+		begin
+			curr_state = HAS0;
+		end else
+		begin
+			curr_state = next_state;
+		end
+	end
+
+	// Set the next state.
+	always_comb
+	begin
+		next_state = HAS0;
+		case (curr_state)
+			HAS0:
+			begin
+				if (i == 0)
+				begin
+					next_state = HAS0;
+				end else // i == 1
+				begin
+					next_state = HAS1;
+				end
+			end
+			HAS1:
+			begin
+				if (i == 0)
+				begin
+					next_state = HAS10;
+				end else // i == 1
+				begin
+					next_state = HAS1;
+				end
+			end
+			HAS10:
+			begin
+				if (i == 0)
+				begin
+					next_state = HAS0;
+				end else // i == 1
+				begin
+					next_state = HAS101;
+				end
+			end
+			HAS101:
+			begin
+				if (i == 0)
+				begin
+					next_state = HAS10;
+				end else // i == 1
+				begin
+					next_state = HAS1011;
+				end
+			end
+			HAS1011:
+			begin
+				if (i == 0)
+				begin
+					next_state = HAS10;
+				end else // i == 1
+				begin
+					next_state = HAS1;
+				end
+			end
+		endcase
+	end
+
+	logic next_o;
+
+	// store output value.
+	always_ff @ (posedge clk, negedge n_rst)
+	begin
+		if (n_rst == '0)
+		begin
+			o = 0;
+		end else
+		begin
+			o = next_o;
+		end
+	end
+
+	//output logic
+	always_comb
+	begin
+		next_o = 0;
+		if (curr_state == HAS1011 && ((next_state == HAS0) || (next_state == HAS1)))
+		begin
+			next_o = 1;
+		end
+	end
 
 endmodule
