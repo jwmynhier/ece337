cefa7c999ef3b07e90e67f9031f60ce2205dbc41
Mentor Graphics <mg111@ecegrid-thin6.ecn.purdue.edu>
2016-09-15 11:12:52 -0400

    Commit working version of (tb_)flex_counter

--- Lab3/source/tb_adder_16bit.sv
+++ Lab3/source/tb_adder_16bit.sv
@@ -28,10 +28,13 @@ module tb_adder_16bit
 	wire	tb_carry_out;
 	
 	// Declare test bench signals
-	integer tb_test_case;
+	//integer tb_test_case;
 	reg [MAX_TEST_BIT:0] tb_test_inputs;
 	reg [NUM_INPUT_BITS:0] tb_expected_outputs;
 	
+	// identify problem signals
+	reg no_match;
+
 	// DUT port map
 	adder_16bit DUT(.a(tb_a), .b(tb_b), .carry_in(tb_carry_in), .sum(tb_sum), .overflow(tb_carry_out));
 	
@@ -44,7 +47,11 @@ module tb_adder_16bit
 	initial
 	begin
 		// Initialize test inputs for DUT
-		tb_test_inputs = '1;
+		tb_test_inputs = '0;
+		//tb_test_case = 'd1;
+
+		no_match = '0;
+		no_match = '1;
 
 		// Wait for a bit to allow this process to catch up with assign statements triggered
 		// by test input assignment above
@@ -59,25 +66,35 @@ module tb_adder_16bit
 		// Check the DUT's Sum output value
 		if(tb_expected_outputs[NUM_INPUT_BITS-1:0] == tb_sum[NUM_INPUT_BITS-1:0])
 		begin
-			$info("Correct Sum value for test case %d!", tb_test_case);
+			$info("Correct Sum value for test case %d!", 1); //tb_test_case);
+			no_match = '0;
 		end
 		else
 		begin
-			$error("Incorrect Sum value for test case %d!", tb_test_case);
+			$error("Incorrect Sum value for test case %d!", 1); //tb_test_case);
+			no_match = 1;
 		end
 		
 		// Check the DUT's Carry Out output value
 		if(tb_expected_outputs[TEST_CARRY_OUT_BIT] == tb_carry_out)
 		begin
-			$info("Correct Carry Out value for test case %d!", tb_test_case);
+			$info("Correct Carry Out value for test case %d!", 1); //tb_test_case);
+			no_match = '0;
 		end
 		else
 		begin
-			$error("Incorrect Carry Out value for test case %d!", tb_test_case);
+			$error("Incorrect Carry Out value for test case %d!", 1); //tb_test_case);
+			no_match = 1;		
 		end
+
+		assert(tb_expected_outputs[NUM_INPUT_BITS-1:0] == tb_sum[NUM_INPUT_BITS-1:0] && (tb_expected_outputs[TEST_CARRY_OUT_BIT] == tb_carry_out))
+			else $error("Incorrect sum for case 1");
+
+		//******************************************************************************
 
 		// Initialize test inputs for DUT
-		tb_test_inputs = 33'hFFFF;
+		tb_test_inputs = 33'h00001FFFD;
+		//tb_test_case = 'd2;
 
 		// Wait for a bit to allow this process to catch up with assign statements triggered
 		// by test input assignment above
@@ -92,25 +109,35 @@ module tb_adder_16bit
 		// Check the DUT's Sum output value
 		if(tb_expected_outputs[NUM_INPUT_BITS-1:0] == tb_sum[NUM_INPUT_BITS-1:0])
 		begin
-			$info("Correct Sum value for test case %d!", tb_test_case);
+			$info("Correct Sum value for test case %d!", 2); //tb_test_case);
+			no_match = '0;
 		end
 		else
 		begin
-			$error("Incorrect Sum value for test case %d!", tb_test_case);
+			$error("Incorrect Sum value for test case %d!", 2); //tb_test_case);
+			no_match = 1;
 		end
 		
 		// Check the DUT's Carry Out output value
 		if(tb_expected_outputs[TEST_CARRY_OUT_BIT] == tb_carry_out)
 		begin
-			$info("Correct Carry Out value for test case %d!", tb_test_case);
+			$info("Correct Carry Out value for test case %d!", 2); //tb_test_case);
+			no_match = '0;
 		end
 		else
 		begin
-			$error("Incorrect Carry Out value for test case %d!", tb_test_case);
+			$error("Incorrect Carry Out value for test case %d!", 2); //tb_test_case);
+			no_match = 1;
 		end
+
+		assert(tb_expected_outputs[NUM_INPUT_BITS-1:0] == tb_sum[NUM_INPUT_BITS-1:0] && (tb_expected_outputs[TEST_CARRY_OUT_BIT] == tb_carry_out))
+			else $error("Incorrect sum for case 2");
+
+		//******************************************************************************
 		
 		// Initialize test inputs for DUT
-		tb_test_inputs = 33'h1AB32AB32;
+		tb_test_inputs = 33'h1FFFF0001;
+		//tb_test_case = 'd3;
 
 		// Wait for a bit to allow this process to catch up with assign statements triggered
 		// by test input assignment above
@@ -125,28 +152,293 @@ module tb_adder_16bit
 		// Check the DUT's Sum output value
 		if(tb_expected_outputs[NUM_INPUT_BITS-1:0] == tb_sum[NUM_INPUT_BITS-1:0])
 		begin
-			$info("Correct Sum value for test case %d!", tb_test_case);
+			$info("Correct Sum value for test case %d!", 3); //tb_test_case);
+			no_match = '0;
 		end
 		else
 		begin
-			$error("Incorrect Sum value for test case %d!", tb_test_case);
+			$error("Incorrect Sum value for test case %d!", 3); //tb_test_case);
+			no_match = 1;
 		end
 		
 		// Check the DUT's Carry Out output value
 		if(tb_expected_outputs[TEST_CARRY_OUT_BIT] == tb_carry_out)
 		begin
-			$info("Correct Carry Out value for test case %d!", tb_test_case);
+			$info("Correct Carry Out value for test case %d!", 3); //tb_test_case);
+			no_match = '0;
 		end
 		else
 		begin
-			$error("Incorrect Carry Out value for test case %d!", tb_test_case);
+			$error("Incorrect Carry Out value for test case %d!", 3); //tb_test_case);
+			no_match = 1;
 		end
+
+		assert(tb_expected_outputs[NUM_INPUT_BITS-1:0] == tb_sum[NUM_INPUT_BITS-1:0] && (tb_expected_outputs[TEST_CARRY_OUT_BIT] == tb_carry_out))
+			else $error("Incorrect sum for case 3");
+
+		//******************************************************************************
+
+		// Initialize test inputs for DUT
+		tb_test_inputs = 33'h1EEE8DDDE;
+		//tb_test_case = 'd4;
+
+		// Wait for a bit to allow this process to catch up with assign statements triggered
+		// by test input assignment above
+		#1;
+			
+		// Calculate the expected outputs
+		tb_expected_outputs = tb_a + tb_b + tb_carry_in;
+		
+		// Wait for DUT to process the inputs
+		#(TEST_DELAY - 1);
+			
+		// Check the DUT's Sum output value
+		if(tb_expected_outputs[NUM_INPUT_BITS-1:0] == tb_sum[NUM_INPUT_BITS-1:0])
+		begin
+			$info("Correct Sum value for test case %d!", 4); //tb_test_case);
+			no_match = '0;
+		end
+		else
+		begin
+			$error("Incorrect Sum value for test case %d!", 4); //tb_test_case);
+			no_match = 1;
+		end
+		
+		// Check the DUT's Carry Out output value
+		if(tb_expected_outputs[TEST_CARRY_OUT_BIT] == tb_carry_out)
+		begin
+			$info("Correct Carry Out value for test case %d!", 4); //tb_test_case);
+			no_match = '0;
+		end
+		else
+		begin
+			$error("Incorrect Carry Out value for test case %d!", 4); //tb_test_case);
+			no_match = 1;
+		end
+
+		assert(tb_expected_outputs[NUM_INPUT_BITS-1:0] == tb_sum[NUM_INPUT_BITS-1:0] && (tb_expected_outputs[TEST_CARRY_OUT_BIT] == tb_carry_out))
+			else $error("Incorrect sum for case 4");
+
+		//******************************************************************************
+
+		// Initialize test inputs for DUT
+		tb_test_inputs = 33'h000020002;
+		//tb_test_case = 'd5;
+
+		// Wait for a bit to allow this process to catch up with assign statements triggered
+		// by test input assignment above
+		#1;
+			
+		// Calculate the expected outputs
+		tb_expected_outputs = tb_a + tb_b + tb_carry_in;
+		
+		// Wait for DUT to process the inputs
+		#(TEST_DELAY - 1);
+			
+		// Check the DUT's Sum output value
+		if(tb_expected_outputs[NUM_INPUT_BITS-1:0] == tb_sum[NUM_INPUT_BITS-1:0])
+		begin
+			$info("Correct Sum value for test case %d!", 5); //tb_test_case);
+			no_match = '0;
+		end
+		else
+		begin
+			$error("Incorrect Sum value for test case %d!", 5); //tb_test_case);
+			no_match = 1;
+		end
+		
+		// Check the DUT's Carry Out output value
+		if(tb_expected_outputs[TEST_CARRY_OUT_BIT] == tb_carry_out)
+		begin
+			$info("Correct Carry Out value for test case %d!", 5); //tb_test_case);
+			no_match = '0;
+		end
+		else
+		begin
+			$error("Incorrect Carry Out value for test case %d!", 5); //tb_test_case);
+			no_match = 1;
+		end
+
+		assert(tb_expected_outputs[NUM_INPUT_BITS-1:0] == tb_sum[NUM_INPUT_BITS-1:0] && (tb_expected_outputs[TEST_CARRY_OUT_BIT] == tb_carry_out))
+			else $error("Incorrect sum for case 5");
+		//******************************************************************************
+		
+		// Initialize test inputs for DUT
+		tb_test_inputs = 33'h0C0C00C08;
+		//tb_test_case = 'd6;
+
+		// Wait for a bit to allow this process to catch up with assign statements triggered
+		// by test input assignment above
+		#1;
+			
+		// Calculate the expected outputs
+		tb_expected_outputs = tb_a + tb_b + tb_carry_in;
+		
+		// Wait for DUT to process the inputs
+		#(TEST_DELAY - 1);
+			
+		// Check the DUT's Sum output value
+		if(tb_expected_outputs[NUM_INPUT_BITS-1:0] == tb_sum[NUM_INPUT_BITS-1:0])
+		begin
+			$info("Correct Sum value for test case %d!", 6); //tb_test_case);
+			no_match = '0;
+		end
+		else
+		begin
+			$error("Incorrect Sum value for test case %d!", 6); //tb_test_case);
+			no_match = 1;
+		end
+		
+		// Check the DUT's Carry Out output value
+		if(tb_expected_outputs[TEST_CARRY_OUT_BIT] == tb_carry_out)
+		begin
+			$info("Correct Carry Out value for test case %d!", 6); //tb_test_case);
+			no_match = '0;
+		end
+		else
+		begin
+			$error("Incorrect Carry Out value for test case %d!", 6); //tb_test_case);
+			no_match = 1;
+		end
+
+		assert(tb_expected_outputs[NUM_INPUT_BITS-1:0] == tb_sum[NUM_INPUT_BITS-1:0] && (tb_expected_outputs[TEST_CARRY_OUT_BIT] == tb_carry_out))
+			else $error("Incorrect sum for case 6");
+		//******************************************************************************
+
+		// Initialize test inputs for DUT
+		tb_test_inputs = 33'h030300307;
+		//tb_test_case = 'd7;
+
+		// Wait for a bit to allow this process to catch up with assign statements triggered
+		// by test input assignment above
+		#1;
+			
+		// Calculate the expected outputs
+		tb_expected_outputs = tb_a + tb_b + tb_carry_in;
+		
+		// Wait for DUT to process the inputs
+		#(TEST_DELAY - 1);
+			
+		// Check the DUT's Sum output value
+		if(tb_expected_outputs[NUM_INPUT_BITS-1:0] == tb_sum[NUM_INPUT_BITS-1:0])
+		begin
+			$info("Correct Sum value for test case %d!", 7); //tb_test_case);
+			no_match = '0;
+		end
+		else
+		begin
+			$error("Incorrect Sum value for test case %d!", 7); //tb_test_case);
+			no_match = 1;
+		end
+		
+		// Check the DUT's Carry Out output value
+		if(tb_expected_outputs[TEST_CARRY_OUT_BIT] == tb_carry_out)
+		begin
+			$info("Correct Carry Out value for test case %d!", 7); //tb_test_case);
+			no_match = '0;
+		end
+		else
+		begin
+			$error("Incorrect Carry Out value for test case %d!", 7); //tb_test_case);
+			no_match = 1;
+		end
+
+		assert(tb_expected_outputs[NUM_INPUT_BITS-1:0] == tb_sum[NUM_INPUT_BITS-1:0] && (tb_expected_outputs[TEST_CARRY_OUT_BIT] == tb_carry_out))
+			else $error("Incorrect sum for case 7");
+		//******************************************************************************
+
+		// Initialize test inputs for DUT
+		tb_test_inputs = 33'h1;
+		//tb_test_case = 'd8;
+
+		// Wait for a bit to allow this process to catch up with assign statements triggered
+		// by test input assignment above
+		#1;
+			
+		// Calculate the expected outputs
+		tb_expected_outputs = tb_a + tb_b + tb_carry_in;
+		
+		// Wait for DUT to process the inputs
+		#(TEST_DELAY - 1);
+			
+		// Check the DUT's Sum output value
+		if(tb_expected_outputs[NUM_INPUT_BITS-1:0] == tb_sum[NUM_INPUT_BITS-1:0])
+		begin
+			$info("Correct Sum value for test case %d!", 8); //tb_test_case);
+			no_match = '0;
+		end
+		else
+		begin
+			$error("Incorrect Sum value for test case %d!", 8); //tb_test_case);
+			no_match = 1;
+		end
+		
+		// Check the DUT's Carry Out output value
+		if(tb_expected_outputs[TEST_CARRY_OUT_BIT] == tb_carry_out)
+		begin
+			$info("Correct Carry Out value for test case %d!", 8); //tb_test_case);
+			no_match = '0;
+		end
+		else
+		begin
+			$error("Incorrect Carry Out value for test case %d!", 8); //tb_test_case);
+			no_match = 1;
+		end
+
+		assert(tb_expected_outputs[NUM_INPUT_BITS-1:0] == tb_sum[NUM_INPUT_BITS-1:0] && (tb_expected_outputs[TEST_CARRY_OUT_BIT] == tb_carry_out))
+			else $error("Incorrect sum for case 8");
+		//******************************************************************************
+
+		// Initialize test inputs for DUT
+		tb_test_inputs = 33'h1FFFFFFFF;
+
+		//tb_test_case = 'd9;
+
+		// Wait for a bit to allow this process to catch up with assign statements triggered
+		// by test input assignment above
+		#1;
+			
+		// Calculate the expected outputs
+		tb_expected_outputs = tb_a + tb_b + tb_carry_in;
+		
+		// Wait for DUT to process the inputs
+		#(TEST_DELAY - 1);
+			
+		// Check the DUT's Sum output value
+		if(tb_expected_outputs[NUM_INPUT_BITS-1:0] == tb_sum[NUM_INPUT_BITS-1:0])
+		begin
+			$info("Correct Sum value for test case %d!", 9); //tb_test_case);
+			no_match = '0;
+		end
+		else
+		begin
+			$error("Incorrect Sum value for test case %d!", 9); //tb_test_case);
+			no_match = 1;
+		end
+		
+		// Check the DUT's Carry Out output value
+		if(tb_expected_outputs[TEST_CARRY_OUT_BIT] == tb_carry_out)
+		begin
+			$info("Correct Carry Out value for test case %d!", 9); //tb_test_case);
+			no_match = '0;
+		end
+		else
+		begin
+			$error("Incorrect Carry Out value for test case %d!", 9); //tb_test_case);
+			no_match = 1;
+		end
+
+		assert(tb_expected_outputs[NUM_INPUT_BITS-1:0] == tb_sum[NUM_INPUT_BITS-1:0] && (tb_expected_outputs[TEST_CARRY_OUT_BIT] == tb_carry_out))
+			else $error("Incorrect sum for case 9");
+		//******************************************************************************
+
+
 	end
 	
 	// Wrap-up process
 	final
 	begin
-		if(NUM_TEST_CASES != tb_test_case)
+		if(NUM_TEST_CASES != 8)
 		begin
 			// Didn't run the test bench through all test cases
 			$display("This test bench was not run long enough to execute all test cases. Please run this test bench for at least a total of %d ns", (NUM_TEST_CASES * TEST_DELAY));
@@ -157,4 +449,5 @@ module tb_adder_16bit
 			$display("This test bench has run to completion");
 		end
 	end
+
 endmodule
---
+++ Lab3/source/tb_flex_counter.sv
@@ -0,0 +1,217 @@
+// $Id: mg111
+// File name:   tb_flex_counter.sv
+// Created:     9/15/2016
+// Author:      Joseph Mynhier
+// Lab Section: 4
+// Version:     1.0  Initial Design Entry
+// Description: scalable counter testbed module.
+
+`timescale 1ns / 10ps
+
+module tb_flex_counter
+();
+	// use default bit length
+	localparam SIZE = 4;
+	
+	localparam CLK_PERIOD = 2.5;
+	reg tb_clk;
+
+	//setup clock
+	always
+	begin
+		tb_clk = 1'b0;
+		#(CLK_PERIOD/2.0);
+		tb_clk = 1'b1;
+		#(CLK_PERIOD/2.0);
+	end
+
+	logic tb_clear;
+	logic tb_n_rst;
+	logic tb_count_enable;
+	logic [SIZE-1:0] tb_rollover_val;
+	logic [SIZE-1:0] tb_count_out;
+	logic tb_rollover_flag;
+
+	flex_counter DUT(.clear(tb_clear), .clk(tb_clk), .count_enable(tb_count_enable), .n_rst(tb_n_rst),
+		.rollover_val(tb_rollover_val), .count_out(tb_count_out), .rollover_flag(tb_rollover_flag));
+
+	initial
+	begin
+
+		$display("Initial!");
+		//Test 1, start count, async reset
+		tb_clear = 0; //inactive
+		tb_count_enable = 0; //inactive
+		tb_rollover_val = 4'b0100;
+
+		@(posedge tb_clk);
+		tb_n_rst	<= 1'b0;
+		@tb_count_out;
+		tb_n_rst	<= 1'b1; 
+
+		@(posedge tb_clk);
+		@(posedge tb_clk);
+
+		tb_count_enable = 1'b1;
+
+		@(posedge tb_clk);
+		@(posedge tb_clk);
+
+		tb_n_rst = 1'b0;
+
+		@(posedge tb_clk);
+		@(posedge tb_clk);
+
+		tb_count_enable = 1'b0;
+
+		if (tb_count_out == '0)
+		begin
+			$info("Test 1: PASS");
+		end else
+		begin
+			$error("Test1: FAIL");
+		end
+		
+		tb_n_rst = 1'b1;
+
+		//Test 2, start count, sync reset
+		tb_count_enable = 1'b1;
+
+		@(posedge tb_clk);
+		@(posedge tb_clk);
+
+		tb_clear = 1'b1;
+
+		@(posedge tb_clk);
+		@(posedge tb_clk);
+
+		tb_count_enable = 1'b0;
+
+		if (tb_count_out == '0)
+		begin
+			$info("Test 1: PASS");
+		end else
+		begin
+			$error("Test1: FAIL");
+		end
+		
+		@(posedge tb_clk);
+		@(posedge tb_clk);
+		
+		tb_clear = 1'b1;
+
+		@(posedge tb_clk);
+		@(posedge tb_clk);
+
+		//Test 3, verify count 1100
+		tb_n_rst = 1'b0;
+
+		@(posedge tb_clk);
+		@(posedge tb_clk);
+
+		tb_n_rst = 1'b1;
+
+		tb_clear = 0; //inactive
+		tb_count_enable = 0; //inactive
+		tb_rollover_val = 4'b1100;
+
+		@(posedge tb_clk);
+		@(posedge tb_clk);
+
+		tb_count_enable = 1'b1;
+
+		@(posedge tb_clk);
+		@(posedge tb_clk);
+
+		@(tb_rollover_flag);
+		if (tb_count_out == 4'b1)
+		begin
+			$info("Test 3: PASS");
+		end else
+		begin
+			$error("Test 3: FAIL");
+		end
+		//Test 4, verify count 0011
+		tb_n_rst = 1'b0;
+
+		@(posedge tb_clk);
+		@(posedge tb_clk);
+
+		tb_n_rst = 1'b1;
+
+		tb_clear = 0; //inactive
+		tb_count_enable = 0; //inactive
+		tb_rollover_val = 4'b0011;
+
+		@(posedge tb_clk);
+		@(posedge tb_clk);
+
+		tb_count_enable = 1'b1;
+
+		@(posedge tb_clk);
+		@(posedge tb_clk);
+
+		@(tb_rollover_flag);
+		if (tb_count_out == 4'b1)
+		begin
+			$info("Test 4: PASS");
+		end else
+		begin
+			$error("Test 4: FAIL");
+		end
+		//Test 5, verify count 1001
+		tb_n_rst = 1'b0;
+
+		@(posedge tb_clk);
+		@(posedge tb_clk);
+
+		tb_n_rst = 1'b1;
+
+		tb_clear = 0; //inactive
+		tb_count_enable = 0; //inactive
+		tb_rollover_val = 4'b1001;
+
+		@(posedge tb_clk);
+		@(posedge tb_clk);
+
+		tb_count_enable = 1'b1;
+
+		@(posedge tb_clk);
+		@(posedge tb_clk);
+		@(tb_rollover_flag);
+		if (tb_count_out == 4'b1)
+		begin
+			$info("Test 5: PASS");
+		end else
+		begin
+			$error("Test 5: FAIL");
+		end
+		//Test 6, verify count 0110
+		tb_n_rst = 1'b0;
+
+		@(posedge tb_clk);
+		@(posedge tb_clk);
+
+		tb_n_rst = 1'b1;
+
+		tb_clear = 0; //inactive
+		tb_count_enable = 0; //inactive
+		tb_rollover_val = 4'b0110;
+
+		@(posedge tb_clk);
+		@(posedge tb_clk);
+
+		tb_count_enable = 1'b1;
+
+		@(posedge tb_clk);
+		@(posedge tb_clk);
+		@(tb_rollover_flag);
+		if (tb_count_out == 4'b1)
+		begin
+			$info("Test 6: PASS");
+		end else
+		begin
+			$error("Test 6: FAIL");
+		end
+	end
+endmodule
