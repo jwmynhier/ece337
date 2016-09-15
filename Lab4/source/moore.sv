2eff96356a7646a7ec3190555967ed0c21322f01
Mentor Graphics <mg111@ee215lnx08.ecn.purdue.edu>
2016-09-15 14:42:20 -0400

    Initial commit for moore.sv

---
+++ Lab4/source/moore.sv
@@ -0,0 +1,17 @@
+// $Id: mg111
+// File name:   moore.sv
+// Created:     9/15/2016
+// Author:      Joseph Mynhier
+// Lab Section: 4
+// Version:     1.0  Initial Design Entry
+// Description: moore model 1101 sequence detector.
+
+module moore
+(
+	input wire clk,
+	input wire n_rst,
+	input wire i,
+	output reg o
+);
+
+endmodule
