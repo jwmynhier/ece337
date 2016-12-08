8446347bbe488bf5d4453740a5f1fb92bfa80192
doodlemoose <dsouza0@purdue.edu>
2016-12-07 21:03:53 -0500

    added tasks for all opcodes

--- tb_GPU.sv
+++ tb_GPU.sv
@@ -29,13 +29,13 @@ module tb_GPU();
 	logic [31:0] tb_PRDATA;
 
 	apb_if apb();
-	assign ahb.PSEL = tb_PSEL;
-	assign ahb.PWDATA = tb_PWDATA； 
-	assign ahb.PWRITE = tb_PWRITE;
+	assign apb.PSEL = tb_PSEL;
+	assign apb.PWDATA = tb_PWDATA; 
+	assign apb.PWRITE = tb_PWRITE;
 	assign tb_PRDATA = apb.PRDATA;
 
 	//DUT
-	2DGPU DUT(
+	GPU DUT(
 		.clk(tb_clk),
 		.n_rst(tb_n_rst),
 		.ahbif(ahb.ahb_m),
@@ -71,6 +71,85 @@ module tb_GPU();
 	end	
 	endtask
 
+	task sendCmd;
+		input logic [2:0] opcode;
+		input logic [23:0] data;
+	begin
+		tb_PSEL = 1'b1;
+		tb_PWRITE = 1'b1;
+		tb_PWDATA = {5'b0, opcode, data};
+	
+		@(negedge tb_clk);
+		@(negedge tb_clk);
+
+		tb_PSEL = 1'b0;
+	end
+	endtask
+
+
+	task setColor;
+		input logic [23:0] color; 
+	begin
+		sendCmd(3'b011,color);
+	end
+	endtask 
+
+	task setStart;
+		input logic [8:0] x;
+		input logic [7:0] y;
+	begin
+		sendCmd(3'b001,{7'b0,x,y});
+	end	
+	endtask
+
+	task setEnd;
+		input logic [8:0] x;
+		input logic [7:0] y;
+	begin
+		sendCmd(3'b010,{7'b0,x,y});
+	end	
+	endtask
+
+	task moveStart;
+		input logic [8:0] x;
+		input logic [7:0] y;
+	begin
+		sendCmd(3'b100,{7'b0,x,y});
+	end	
+	endtask
+
+	task moveEnd;
+		input logic [8:0] x;
+		input logic [7:0] y;
+	begin
+		sendCmd(3'b101,{7'b0,x,y});
+	end	
+	endtask
+
+	task flip;
+	begin
+		sendCmd(3'b111,24'b0);
+	end
+	endtask 
+
+	task draw;
+	begin
+		sendCmd(3'b110,24'b0);
+	end
+	endtask 
+
+	task clear;
+	begin
+		sendCmd(3'b000,24'b0);
+	end
+	endtask 
+
+	initial
+	begin
+		reset;
+	
+		$info("sending color red");	
+		setColor(24'hFF0000);
+	end
 
 endmodule 
-
