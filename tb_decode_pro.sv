7b49f36c471afb2ac0de5b353db4a2ee9bc0f32a
nithin900712 <nithin900712@users.noreply.github.com>
2016-12-14 22:19:35 -0500

    Update tb_decode_pro.sv

--- tb_decode_pro.sv
+++ tb_decode_pro.sv
@@ -66,12 +66,155 @@ module tb_decode_pro();
 	end	
 	endtask
 
+	task sendCmd;
+		input logic [2:0] opcode;
+		input logic [23:0] data;
+	begin
+		tb_command_bus = {5'b0, opcode, data};
+		tb_penable = 1'b1;
+		tb_render_enable = 1'b0;
+		@(negedge tb_clk);
+		@(negedge tb_clk);
+
+		tb_penable = 1'b0;
+	end
+	endtask
+
+	task setColor;
+		input logic [23:0] color; 
+	begin
+		$info("sending color red");			
+		sendCmd(3'b011,color);
+	end
+	endtask 
+
+	task setStart;
+		input logic [8:0] x;
+		input logic [7:0] y;
+	begin
+		$info("set start");		
+		sendCmd(3'b001,{7'b0,x,y});
+	end	
+	endtask
+
+	task setEnd;
+		input logic [8:0] x;
+		input logic [7:0] y;
+	begin
+		$info("set end");	
+		sendCmd(3'b010,{7'b0,x,y});
+	end	
+	endtask
+
+	task moveStart;
+		input logic [8:0] x;
+		input logic [7:0] y;
+	begin
+		sendCmd(3'b100,{7'b0,x,y});
+	end	
+	endtask
+
+	task moveEnd;
+		input logic [8:0] x;
+		input logic [7:0] y;
+	begin
+		sendCmd(3'b101,{7'b0,x,y});
+	end	
+	endtask
+
+	task flip;
+	begin
+		sendCmd(3'b111,24'b0);
+	end
+	endtask 
+
+	task draw;
+	begin
+		$info("draw line");
+		sendCmd(3'b110,24'b0);
+	end
+	endtask 
+
+	task clear;
+	begin
+		sendCmd(3'b000,24'b0);
+	end
+	endtask 
 	
 
 	initial
 	begin
 		reset;
+		
+		setStart(9'd0, 8'd0);
+		#(10*CLK_PERIOD);
+		assert(tb_start == 16'b0)
+	 	 $info("Test 1 has correctly set start ");
+		else
+	 	 $error("Error start");
+		setEnd(9'd319, 8'd239);
+		#(10*CLK_PERIOD);
+		assert(tb_end1 == {9'd319,8'd239})
+	 	 $info("Test 2 has correctly set end ");
+		else
+	 	 $error("Error end");	
+		setColor(24'hFF0000);
+		#(10*CLK_PERIOD);
+		assert(tb_color == 24'hFF0000)
+	 	 $info("Test 3 has correctly set color ");
+		else
+	 	 $error("Error color");
+		
+		draw;
+		
+		assert(tb_received_op == 1'b1)
+	 	 $info("Test 5 has correctly set received_op");
+		else
+	 	 $error("Error rec_op");
+		
+		#(3*CLK_PERIOD);
+		assert(tb_op == 3'b110)
+	 	 $info("Test 4 has correctly set op");
+		else
+	 	 $error("Error op");
 
+		flip;
+		
+		assert(tb_flip_buffer == 1'b1)
+	 	 $info("Test 6 has correctly set flip_buffer");
+		else
+	 	 $error("Error flip");
+		
+		#(3*CLK_PERIOD);
+		assert(tb_op == 3'b111)
+	 	 $info("Test 6 has correctly set op");
+		else
+	 	 $error("Error op");
+
+		
+		clear;
+		
+		#(3*CLK_PERIOD);
+		assert(tb_op == 3'b000)
+	 	 $info("Test 7 has correctly set op");
+		else
+	 	 $error("Error op");
+
+		moveStart(9'd0, 8'd0);
+		#(10*CLK_PERIOD);
+		assert(tb_start == 16'b0)
+	 	 $info("Test 8 has correctly moved start ");
+		else
+	 	 $error("Error start");
+		moveEnd(9'd0, 8'd0);
+		#(10*CLK_PERIOD);
+		assert(tb_end1 == {9'd319,8'd239})
+	 	 $info("Test 9 has correctly move end ");
+		else
+	 	 $error("Error end");	
+		
+
+		
 		
 		
 	end
